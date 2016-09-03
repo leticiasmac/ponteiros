7940c055e20f2693f4f9b3db3705b9da8f08c608
leticiasmac <leticiasmac@gmail.com>
2016-09-02 21:32:43 -0300

    programa funcional

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
+//*************************************
+//*              PONTEIROS            *
+//* Autora: Letícia Machado           *
+//* Aula: Laboratório de Algoritmos 2 *
+//* Data: 02/09/2016                  *
+//* Antonio Meneghetti Faculdade      *
+//*************************************
+
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int numero = 2;
+int *ponteiro = &numero;
+
+int main(int argc, char** argv) 
+{
+	printf("insira um numero: "); //pede um numero ao usuario
+	
+//	scanf("%i", &numero);
+	scanf("%i", ponteiro); //salva o valor
+
+//	printf("O numero inserido foi: %i!\n", numero);
+printf("O numero inserido foi: %i!\n", *ponteiro); //mostra o valor inserido na tela
+	
 	return 0;
 }
