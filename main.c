2e0b66dfc94bb02cda27a13c445b75c3d2bc0ed2
leticiasmac <leticiasmac@gmail.com>
2016-09-09 22:22:35 -0300

    programa funcional

--- main.c
+++ main.c
@@ -2,29 +2,43 @@
 //*              PONTEIROS            *
 //* Autora: Letícia Machado           *
 //* Aula: Laboratório de Algoritmos 2 *
-//* Data: 02/09/2016                  *
+//* Data: 09/09/2016                  *
 //* Antonio Meneghetti Faculdade      *
 //*************************************
 
 #include <stdio.h>
 
+typedef struct 
+{
+	int Indice;
+	char Nome [50];
+	char Telefone [15];
+}
+	entrada;
+
 int main ()
 {
 	//criacao e inicializacao das variaveis e ponteiros
-	//char minha_string [] = {"1234567890"};
-	char minha_string [] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-	char *meu_ponteiro = &minha_string;
+	entrada pagina_01;
+	entrada *ponteiro = &pagina_01;
 	
-	//exibe o valor contido em cada posicao da string a partir do endereco contido no ponteiro
+	//pagina_01.Indice = 1;
+	//pagina_01.Nome = {"Jonas Tadeu Reichert"};
+	//pagina_01.Telefone = {"(55) 9903-4395"}
 	
-	do
-	{
-		printf("\n o meu_ponteiro aponta para o valor: %c\n", *meu_ponteiro);
-		meu_ponteiro ++;
-	}
-	while (*meu_ponteiro != '0');
+	printf("\n Insira o indice: ");
+	//scanf("%i", &pagina_01.Indice);
+	scanf("%i", &ponteiro->Indice);
+
+	printf("\n Insira o nome: ");
+	scanf("%s", &ponteiro->Nome);
+
+	printf("\n Insira o telefone: ");
+	scanf("%c", &ponteiro->Telefone);
 	
-	printf("\n o tamanho de minha_string eh: %i\n", sizeof (minha_string));
+	printf("\nIndice: %i\n", pagina_01.Indice);
+	printf("\nNome: %s\n", pagina_01.Nome);
+	printf("\nTelefone: %s\n", pagina_01.Telefone);
 	
 	//retorna 0
 	return 0;
